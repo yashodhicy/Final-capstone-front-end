ef4243810e493e61f9ad23e1fc76c3084799c08f
brhanuhailu <hailubrhanu29@gmail.com>
2023-09-23 15:29:19 -0700

    fix eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-//import './App.css'
 import Navbar from './components/Navbar'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <Router>
