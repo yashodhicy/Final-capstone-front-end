f1cc9e7d468fac91fb8c679bb19459c503cf34ea
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-29 13:11:33 +0100

    Add error element

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,17 @@ import "./App.css";
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Outlet />}>
+      <Route
+        errorElement={<div>This is the error page</div>}
+        path="/"
+        element={<Outlet />}
+      >
         <Route path="/" element={<Root />}>
-
           {/* Add navigation tabs here */}
           <Route path="/reserve" element={<Reservation />} />
         </Route>
-        <Route path="/login" element={ <h1>I&apos;m the login page</h1>} />
-        <Route path="/signup" element={ <h1>I&apos;m the Sign up page</h1>} />
+        <Route path="/login" element={<h1>I&apos;m the login page</h1>} />
+        <Route path="/signup" element={<h1>I&apos;m the Sign up page</h1>} />
       </Route>
     )
   );
