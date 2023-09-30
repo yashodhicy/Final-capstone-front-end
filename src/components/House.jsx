16129b626b18c412a23a2aad88c9dd55e0b1c1fb
brhanuhailu <hailubrhanu29@gmail.com>
2023-09-29 21:25:00 -0700

    House componets

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import './componentsCss/houses.css';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getHouses } from '../Redux/HouseSlice';
