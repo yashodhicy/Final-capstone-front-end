97424a516e080bc1f80c35974eab9872dbe3551b
brhanuhailu <hailubrhanu29@gmail.com>
2023-09-30 05:27:28 -0700

    store reducer

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import HouseSlice from './HouseSlice';
+import deleteReducer from './deleteSlice';
 
 
 const store = configureStore({
   reducer: {
     Houses: HouseSlice,
+    delete : deleteReducer,
   },
 });
 
