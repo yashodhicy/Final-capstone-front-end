c03211c4f0bf9ab01fa05cd6ac6164ed8a6483c8
brhanuhailu <hailubrhanu29@gmail.com>
2023-09-29 21:23:47 -0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Houses from './components/House'
+import AddHouse from './components/AddHouse'
+
 
 function App() {
+
 
   return (
       <>
         <Navbar />
+        <Navbar />
         <Routes>
           <Route path='/' element= {<Houses />}></Route>
+          <Route path='/add' element= {<AddHouse />}></Route>
         </Routes>
         
     </>
