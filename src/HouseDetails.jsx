677daf20d50b7a42ca92f795e9fb82f9aa55c4a1
brhanuhailu <hailubrhanu29@gmail.com>
2023-09-30 05:26:52 -0700

    House detail to delete

--- src/HouseDetails.jsx
+++ src/HouseDetails.jsx
@@ -1,13 +1,29 @@
-import { useSelector } from "react-redux";
-import {  useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
 
 const HouseDetails = () => {
+  const dispatch = useDispatch();
   const { houseId } = useParams();
+  const navigate = useNavigate();
+  const userId =1;
 
   const houses = useSelector((state) => state.Houses).houses;
   const houseIdNumber = parseInt(houseId, 10);
   const house = houses.find((house) => houseIdNumber === house.id);
   console.log(house);
+
+  const [deleteMessage, setDeleteMessage] = useState("");
+  
+  const handleDelete = () => {
+      dispatch(deleteHouseThunk(houseId)).then((action) => {
+        const message = 'Successfully Deleted';
+        navigate(`/?message=${encodeURIComponent(message)}`);
+    })
+    .catch((error) => {
+      const message = 'Error: Unable to delete';
+    });
+  };
 
   if (house) {
   return (
@@ -29,6 +45,14 @@ const HouseDetails = () => {
           </button>
         </div> 
       </div>
+      {house.user_id === userId &&
+      (
+      <div className="delete">
+        <button type="button" className="delete" onClick={handleDelete}>
+          Delete House
+        </button>
+      </div>
+      )}
     </div>
   );
   } 
