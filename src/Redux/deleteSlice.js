62fb1a3d8377b084d4f8dd0cfb02d20281ec3b7b
brhanuhailu <hailubrhanu29@gmail.com>
2023-10-01 01:26:59 -0700

    Delete comp

---
+++ src/Redux/deleteSlice.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios';
+
+
+export const deleteHouseThunk = createAsyncThunk('houses/delete', async (houseId) => {
+  try {
+    const response = await axios.delete(`https://house-rental-8mh7.onrender.com/api/v1/houses/${houseId}?access-token=Qk5AG4wl5r7ZMA1GzLE0ZA&client=DAHOeSDbu-bxDebj04bxfA&uid=yash2@email.com`);
+    if (!response.ok) {
+      throw new Error('Delete request failed');
+    }
+    return response.error;
+  } 
+  catch (error) {
+    
+    throw error;
+  }
+});
+  
+
+const initialState = {
+    status: 'idle',
+    data: null,
+    error: null,
+};
+
+const deleteSlice = createSlice({
+    name: 'deletehouse',
+    initialState,
+    reducers: [],
+    extraReducers: builder => {
+        builder
+          .addCase(deleteHouseThunk.pending, (state) => {
+            state.status = 'loading'
+          })
+          .addCase(deleteHouseThunk.fulfilled, (state, action) => {
+            state.status = 'succeeded'
+            state.data = action.payload;
+            console.log(state.status);
+          })
+          .addCase(deleteHouseThunk.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message; // Store the error message
+            console.error(action.error);
+          });
+    }
+})
+
+
+export default deleteSlice.reducer;
+
---
+++ src/components/Delete.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteHouseThunk } from '../Redux/deleteSlice.js';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { userHouses } from '../Redux/HouseSlice.js';
+
+const DeleteHouse = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+  dispatch(userHouses());
+  },[dispatch]);
+
+  const userhouses = useSelector((state) => state.Houses.userhouses);
+  const navigate = useNavigate();
+
+  const handleDelete = (houseId) => {
+    dispatch(deleteHouseThunk(houseId)).then((action) => {
+      navigate('/delete');
+      console.log('deleteHouseThunk action:', action);
+    });
+  };
+
+  return (
+    <div>
+      <section className="houses">
+      <div className="header">
+        <h2>Need a House to Delete ?</h2>
+        <p>checkout our available Houses</p>
+      </div>
+
+      <div className="all">
+       {userhouses.map((e) => (
+          <div className="each" key={e.id}>
+            <div className="image">
+              <img src={e.image} alt={e.name} crossOrigin="anonymous | use-credentials" />
+            </div>
+            <Link to={`/houses/${e.id}`} className="link">{e.name}</Link>
+            <p>
+              {e.description.split(' ').slice(0, 15).join(' ')}
+              . . .
+            </p>
+            <ul>
+              <li><i className="fa-brands fa-twitter" /></li>
+              <li><i className="fa-brands fa-facebook-f" /></li>
+              <li><i className="fa-brands fa-instagram" /></li>
+            </ul>
+
+            <div>
+              <button type = "button" className="btn" onClick={() => handleDelete(e.id)}>Delete House</button>
+            </div>
+
+          </div>
+
+        ))}
+      </div>
+      {/* <div className="btn">
+        <button className="prev" type="button" disabled={prevdis} onClick={prev}>
+          <i className="fa-solid fa-caret-left" />
+        </button>
+        <button className="next" type="button" disabled={buttondis} onClick={next}>
+          <i className="fa-solid fa-caret-right" />
+        </button>
+      </div> */}
+    </section>
+    </div>
+  );
+}
+
+export default DeleteHouse;
