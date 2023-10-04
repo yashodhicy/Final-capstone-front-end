5f2b79fd1d8f51b9f98f871cf8378bdd417c6af0
brhanuhailu <hailubrhanu29@gmail.com>
2023-10-03 23:42:00 -0700

    Test add house component

---
+++ src/components/__tests__/AddHouse.test.jsx
@@ -0,0 +1,20 @@
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../../Redux/store';
+import AddHouse from '../AddHouse';
+
+describe('AddHouse Component', () => {
+    test ('it renders correctly', () => {
+        const tree = renderer
+        .create(
+            <Provider store={store}>
+                <Router>
+                    <AddHouse />
+                </Router>
+            </Provider>
+        ).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    })
+});
