9572e63365574641861e781bb98575830d0dffcf
brhanuhailu <hailubrhanu29@gmail.com>
2023-10-03 23:57:04 -0700

    Update test message

--- src/components/__tests__/AddHouse.test.jsx
+++ src/components/__tests__/AddHouse.test.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../../Redux/store';
 import AddHouse from '../AddHouse';
 
-describe('AddHouse Component', () => {
+describe('Test AddHouse Component', () => {
     test ('it renders correctly', () => {
         const tree = renderer
         .create(
